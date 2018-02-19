d3b90d8712722fdcb6b77eb12e868af586b7f7a1
Alexey <alexey.d@ucoz-team.net>
2018-02-19 16:30:18 +0300

    Немного поправил стили

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,7 @@
 import { Routes } from '@angular/router';
 import { NoContentComponent } from './no-content';
-import { LoginPageComponent } from './auth/containers/login.component';
-import { TemplatesPageComponent } from './templates/containers/templates.component';
-import { AuthGuard } from './auth/guards/auth';
-import { NoAuthGuard } from './auth/guards/no-auth';
 
 export const ROUTES: Routes = [
 	{ path: '', redirectTo: '/login', pathMatch: 'full' },
-	// { path: 'login', component: LoginPageComponent, canActivate: [ NoAuthGuard ] },
-	// { path: 'templates', component: TemplatesPageComponent, canActivate: [ AuthGuard ] },
 	{ path: '**', component: NoContentComponent }
 ];
