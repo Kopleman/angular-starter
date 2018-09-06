4b54ab89255665a5b433a0ed9574f63d5c858cdb
Alexey <alexey.d@ucoz-team.net>
2018-09-06 15:02:27 +0300

    спека для сервиса схемы

---
+++ src/app/i18n-scheme/services/template-i18n-data.spec.ts
@@ -0,0 +1,86 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { APP_DI_CONFIG, AppConfigModule } from '../../config.module';
+import { Api } from '../../core/services/api';
+import { TemplatesI18nData } from './template-i18n-data';
+import { IUserRole } from '../../users/models/user';
+
+describe('i18n service', () => {
+	let httpClient: HttpClient;
+	let httpTestingController: HttpTestingController;
+	let api: Api;
+	let templatesI18nData: TemplatesI18nData;
+	const host = APP_DI_CONFIG.host;
+
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			// Import the HttpClient mocking services
+			imports: [HttpClientTestingModule, AppConfigModule],
+			// Provide the service-under-test and its dependencies
+			providers: [Api, TemplatesI18nData]
+		});
+
+		httpClient = TestBed.get(HttpClient);
+		httpTestingController = TestBed.get(HttpTestingController);
+		api = TestBed.get(Api);
+		templatesI18nData = TestBed.get(TemplatesI18nData);
+	});
+
+	afterEach(() => {
+		// After every test, assert that there are no more pending requests.
+		httpTestingController.verify();
+	});
+
+	describe('#getTemplates', () => {
+		let expectedAnswer: Array<{ [index: string]: string }>;
+		beforeEach(() => {
+			expectedAnswer = [{ _id: 'mock', title: 'mock' }] as Array<{ [index: string]: string }>;
+		});
+		it('should return expected answer (called once)', () => {
+			templatesI18nData
+				.getTemplates()
+				.subscribe(
+					answer =>
+						expect(answer).toEqual(
+							expectedAnswer,
+							'should return expected locales'
+						),
+					fail
+				);
+
+			const req = httpTestingController.expectOne(
+				`${host}admin/rest/i18n/templates`
+			);
+			expect(req.request.method).toEqual('GET');
+
+			req.flush(expectedAnswer);
+		});
+	});
+
+	describe('#getActiveLocalesHash', () => {
+		let expectedAnswer: { [index: string]: string };
+		beforeEach(() => {
+			expectedAnswer = { ru: 'Русский', en: 'English' } as { [index: string]: string };
+		});
+		it('should return expected answer (called once)', () => {
+			templatesI18nData
+				.getActiveLocalesHash()
+				.subscribe(
+					answer =>
+						expect(answer).toEqual(
+							expectedAnswer,
+							'should return expected locales'
+						),
+					fail
+				);
+
+			const req = httpTestingController.expectOne(
+				`${host}admin/rest/i18n/activeLocalesHash`
+			);
+			expect(req.request.method).toEqual('GET');
+
+			req.flush(expectedAnswer);
+		});
+	});
+});
